f47d8164c36a0c79165a96c0cbddc10d5f9ed895
archana <ms.archanacusat2015@gmail.com>
2016-02-18 10:18:16 +0530

    After Database operation

---
+++ app/src/main/java/com/robosoft/archana/recyclerviewassignment/Interfaces/Communicator.java
@@ -0,0 +1,12 @@
+package com.robosoft.archana.recyclerviewassignment.Interfaces;
+
+import com.robosoft.archana.recyclerviewassignment.Modal.ProductList;
+
+import java.util.ArrayList;
+
+/**
+ * Created by archana on 12/2/16.
+ */
+public interface Communicator {
+    public void onClickOfAddButton(ArrayList<ProductList> arrayList);
+}
